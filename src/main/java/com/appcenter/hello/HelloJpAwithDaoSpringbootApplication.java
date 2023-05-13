3043c242f74886cc3f37f076ec4823da7db081df
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-05-13 17:08:23 +0900

    [Feat] Spring 버전 변경
    스웨거 2.0 적용을 위한 Spring 버전 다운그레이드

---
+++ src/main/java/com/appcenter/hello/HelloJpAwithDaoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.appcenter.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJpAwithDaoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJpAwithDaoSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appcenter/hello/HelloJpAwithDaoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appcenter.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJpAwithDaoSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
