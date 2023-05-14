cc605d107071ccdd9b221b0dc1581fa691773bf4
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-05-14 12:48:03 +0900

    [Feat] MemberDAOImpl에 Update, Delete 기능 추가
    Update: number를 받아와서 이름을 수정할 수 있는 기능 (추후 DTO를 받아와서 전체를 수정할 수 있는 메소드로 수정 예정)
    Delete: number를 받아와서 삭제하는 기능

--- src/main/java/com/appcenter/hello/data/dao/impl/MemberDAOImpl.java
+++ src/main/java/com/appcenter/hello/data/dao/impl/MemberDAOImpl.java
@@ -6,6 +6,8 @@ import com.appcenter.hello.data.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 // MemberDAO의 구현체 클래스
 @Component
 public class MemberDAOImpl implements MemberDAO {
@@ -36,11 +38,36 @@ public class MemberDAOImpl implements MemberDAO {
 
     @Override
     public Member updateMember(Long number, String name) throws Exception {
-        return null;
+        // select로 정보 가져오기
+        // Optional NPE을 방지할 수 있는 클래스
+        // NPE과 관련된 다양한 메소드를 제공해준다.
+        Optional<Member> selectedMember = memberRepository.findById(number);
+
+        Member updateMember;
+        if (selectedMember.isPresent()) {
+            Member member = selectedMember.get();
+
+            member.setName(name);
+
+            updateMember = memberRepository.save(member);
+        } else {
+            // 존재하지 않을 경우 Exception
+            throw new Exception();
+        }
+
+        return updateMember;
     }
 
     @Override
     public void deleteProduct(Long number) throws Exception {
+        Optional<Member> selectedMember = memberRepository.findById(number);
 
+        if (selectedMember.isPresent()) {
+            Member member = selectedMember.get();
+
+            memberRepository.delete(member);
+        } else {
+            throw new Exception();
+        }
     }
 }
